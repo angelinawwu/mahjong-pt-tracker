ee5202f57388d838620856e7cc03ed33dc44e7cc
angelinawwu <angelinawu05@gmail.com>
2026-07-14 12:00:20 -0700

    tilted tiles

--- src/components/MahjongTile3D.tsx
+++ src/components/MahjongTile3D.tsx
@@ -16,74 +16,79 @@ export function MahjongTile3D({ tile, className, ...props }: MahjongTile3DProps)
         style={{ transformStyle: "preserve-3d" }}
         {...props}
       >
-        {/* Front */}
         <div
-          className="absolute inset-0 rounded-[2px]"
-          style={{ transform: "translateZ(33.33cqi)" }}
+          className="relative w-full h-full"
+          style={{ transformStyle: "preserve-3d", transform: "rotateX(25deg) rotateY(-20deg) rotateZ(5deg)" }}
         >
-          <img
-            src={`/tiles/${tile}.webp`}
-            alt={tile}
-            className="w-full h-full rounded-[2px] object-fill"
+          {/* Front */}
+          <div
+            className="absolute inset-0 rounded-[2px]"
+            style={{ transform: "translateZ(33.33cqi)" }}
+          >
+            <img
+              src={`/tiles/${tile}.webp`}
+              alt={tile}
+              className="w-full h-full rounded-[2px] object-fill"
+            />
+          </div>
+          
+          {/* Back */}
+          <div
+            className="absolute inset-0 rounded-[2px]"
+            style={{
+              transform: "rotateY(180deg) translateZ(33.33cqi)",
+              backgroundColor: "var(--jade)",
+            }}
+          />
+
+          {/* Left */}
+          <div
+            className="absolute top-0 left-[50%]"
+            style={{
+              width: "66.66cqi",
+              height: "100%",
+              marginLeft: "-33.33cqi",
+              transform: "rotateY(-90deg) translateZ(50cqi)",
+              background: "linear-gradient(to right, var(--jade) 50%, white 50%)",
+            }}
+          />
+
+          {/* Right */}
+          <div
+            className="absolute top-0 left-[50%]"
+            style={{
+              width: "66.66cqi",
+              height: "100%",
+              marginLeft: "-33.33cqi",
+              transform: "rotateY(90deg) translateZ(50cqi)",
+              background: "linear-gradient(to right, white 50%, var(--jade) 50%)",
+            }}
+          />
+
+          {/* Top */}
+          <div
+            className="absolute left-0 top-[50%]"
+            style={{
+              width: "100%",
+              height: "66.66cqi",
+              marginTop: "-33.33cqi",
+              transform: "rotateX(90deg) translateZ(66.66cqi)",
+              background: "linear-gradient(to bottom, var(--jade) 50%, white 50%)",
+            }}
+          />
+
+          {/* Bottom */}
+          <div
+            className="absolute left-0 top-[50%]"
+            style={{
+              width: "100%",
+              height: "66.66cqi",
+              marginTop: "-33.33cqi",
+              transform: "rotateX(-90deg) translateZ(66.66cqi)",
+              background: "linear-gradient(to bottom, white 50%, var(--jade) 50%)",
+            }}
           />
         </div>
-        
-        {/* Back */}
-        <div
-          className="absolute inset-0 rounded-[2px]"
-          style={{
-            transform: "rotateY(180deg) translateZ(33.33cqi)",
-            backgroundColor: "var(--jade)",
-          }}
-        />
-
-        {/* Left */}
-        <div
-          className="absolute top-0 left-[50%]"
-          style={{
-            width: "66.66cqi",
-            height: "100%",
-            marginLeft: "-33.33cqi",
-            transform: "rotateY(-90deg) translateZ(50cqi)",
-            background: "linear-gradient(to right, var(--jade) 50%, white 50%)",
-          }}
-        />
-
-        {/* Right */}
-        <div
-          className="absolute top-0 left-[50%]"
-          style={{
-            width: "66.66cqi",
-            height: "100%",
-            marginLeft: "-33.33cqi",
-            transform: "rotateY(90deg) translateZ(50cqi)",
-            background: "linear-gradient(to right, white 50%, var(--jade) 50%)",
-          }}
-        />
-
-        {/* Top */}
-        <div
-          className="absolute left-0 top-[50%]"
-          style={{
-            width: "100%",
-            height: "66.66cqi",
-            marginTop: "-33.33cqi",
-            transform: "rotateX(90deg) translateZ(66.66cqi)",
-            background: "linear-gradient(to bottom, var(--jade) 50%, white 50%)",
-          }}
-        />
-
-        {/* Bottom */}
-        <div
-          className="absolute left-0 top-[50%]"
-          style={{
-            width: "100%",
-            height: "66.66cqi",
-            marginTop: "-33.33cqi",
-            transform: "rotateX(-90deg) translateZ(66.66cqi)",
-            background: "linear-gradient(to bottom, white 50%, var(--jade) 50%)",
-          }}
-        />
       </motion.div>
     </div>
   );
